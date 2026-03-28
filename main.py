9e8b288cfa77df72c7a95a02579677d0547cdce5
Sergey Pryadko <sergeypryadko@gmail.com>
2026-03-28 11:32:02 +0300

    описание_коммита

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from thing import Thing
 import random
+
+from thing import Thing
+
 # Шаг 1 - создаем произвольное количество вещей с различными параметрами, процент защиты не должен превышать 10%(0.1). 
 #    Сортируем по проценту защиты, по возрастанию;
 MAX_TOTAL_THING = 100
@@ -7,18 +9,19 @@ MAX_ATTACK = 20
 MAX_HEALTH = 20
 
 total_thing = random.randint(1, MAX_TOTAL_THING)
-names = ["sword", "helmet", "cloak", "magic_wand", "book_of_spells"]
+names = ["меч", "шлем", "плащ", "волшебная палочка", "книга заклинаний"]
 
 
 things = [
     Thing(
-        name=names[random.randint(0, len(names))],
+        name=names[random.randint(0, len(names) - 1)],
         armor_percent=random.randint(0, 10),
         attack=random.randint(0, MAX_ATTACK),
         health=random.randint(0, MAX_HEALTH)
     )
     for _ in range(total_thing)
 ]
-
-
+things.sort(key=lambda x: x.armor_percent, reverse=False)
+for thing in things:
+    print(thing)
 
--- thing.py
+++ thing.py
@@ -1,7 +1,12 @@
-#Класс содержит в себе следующие параметры - название, процент защиты, атаку и жизнь
 class Thing():
+    """
+    Класс, представляющий игровую вещь.
+    """
     def __init__(self, name, armor_percent, attack, health):
         self.name = name
         self.armor_percent = armor_percent
         self.attack = attack
         self.health = health
+
+    def __str__(self):
+        return f'Вещь: имя - {self.name}, броня - {self.armor_percent}%, атака - {self.attack}, здоровье - {self.health}'
